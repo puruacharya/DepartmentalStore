214e5861df8ea2e178fdf7e8bee2cf6ed63b1633
puruacharya <pururavaacharya@gmail.com>
2017-02-11 13:41:49 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class DepartmentalStore{
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    int a;
+    System.out.prinltn("Enter a number");
+    a=in.nextInt();
+    System.out.println(a);
+  }
+}
